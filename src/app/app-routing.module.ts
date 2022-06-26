7efe5fb3418487e64ddc8a1103c769cb9716701d
Michelle Athayde <michelle.estevinho@hotmail.com>
2022-06-26 15:34:09 -0300

    Criação do componente initial-page e rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InitialPageComponent } from './page/initial-page/initial-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InitialPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
