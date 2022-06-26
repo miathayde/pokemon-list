2ea9f0a511283584c8ce5fef1a68092a4b37e7a5
Michelle Athayde <michelle.estevinho@hotmail.com>
2022-06-26 19:32:45 -0300

    Implementações iniciais da lista de pokemons

--- src/app/page/initial-page/initial-page.component.ts
+++ src/app/page/initial-page/initial-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonListModel } from 'src/app/models/pokemon-list';
+import { PokemonListService } from 'src/app/service/pokemon-list.service';
 
 @Component({
   selector: 'app-initial-page',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InitialPageComponent implements OnInit {
 
-  constructor() { }
+  minElement: number = 20;
+  maxElement: number;
+  pokemonList: PokemonListModel = new PokemonListModel();
+  pokemonImage = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png';
+
+  constructor(private service: PokemonListService) { }
 
   ngOnInit(): void {
+    this.getList(0);
   }
 
+  getList(quantity: number) {
+    this.service.getList(quantity).subscribe(
+      result => {
+        this.pokemonList = result;
+        this.maxElement = this.pokemonList.count;
+        console.log(this.pokemonList)
+      }
+    );
+  }
 }
