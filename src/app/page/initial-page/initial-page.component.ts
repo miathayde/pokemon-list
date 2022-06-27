5f557cb3e1b3bfa7799b172ee036bc48ea9782d5
Michelle Athayde <michelle.estevinho@hotmail.com>
2022-06-27 20:10:33 -0300

    Funcionalidade dos botões das páginas

--- src/app/page/initial-page/initial-page.component.ts
+++ src/app/page/initial-page/initial-page.component.ts
@@ -9,19 +9,38 @@ import { PokemonListService } from 'src/app/service/pokemon-list.service';
 })
 export class InitialPageComponent implements OnInit {
 
-  minElement: number = 20;
+  minElement: number = 1;
   maxElement: number;
+  currentPage: number = 0;
+  totalPages: number;
+  totalPagesNotRound: number;
   pokemonList: PokemonListModel = new PokemonListModel();
   pokemonImage = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png';
 
   constructor(private service: PokemonListService) { }
 
   ngOnInit(): void {
-    this.getList(0);
+    this.getList();
   }
 
-  getList(quantity: number) {
-    this.service.getList(quantity).subscribe(
+  getList() {
+    this.service.getList(this.currentPage).subscribe(
+      result => {
+        this.pokemonList = result;
+        this.maxElement = this.pokemonList.count;
+
+        this.totalPagesNotRound = this.maxElement / 20;
+        var roundNumber = Math.round(this.maxElement / 20);
+        this.totalPages = roundNumber < this.totalPagesNotRound ? roundNumber + 1 : roundNumber;
+        console.log(this.pokemonList)
+      }
+    );
+  }
+
+  changePage(page: string) {
+    page == 'previous' ? this.currentPage -= 20 : this.currentPage += 20;
+    
+    this.service.getList(this.currentPage).subscribe(
       result => {
         this.pokemonList = result;
         this.maxElement = this.pokemonList.count;
@@ -29,4 +48,26 @@ export class InitialPageComponent implements OnInit {
       }
     );
   }
+
+  firstOrLastPage(page: string) {
+    page == 'first' ? this.currentPage = 0 : this.currentPage = this.maxElement - this.getDecimalNumber(this.totalPagesNotRound);
+
+    this.service.getList(this.currentPage).subscribe(
+      result => {
+        this.pokemonList = result;
+        this.maxElement = this.pokemonList.count;
+        console.log(this.pokemonList)
+      }
+    );
+  }
+
+  getDecimalNumber(item: number): number {
+    let itemString = String(item);
+    
+    const getDecimal = itemString.substring(itemString.indexOf('.'), itemString.length);
+    item = Number(getDecimal.replace('.', ''));
+
+    return item;
+  }
+  
 }
